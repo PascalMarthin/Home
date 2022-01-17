ae5eafe7b7b9fcf42a6c1f2cdb0d01dae35420ce
장중혁 <wndgur98@gmail.com>
2022-01-17 20:42:39 +0900

    장중혁 : 220117HomeWork

---
+++ HomeWork220117/HomeWork220117.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+int CountLen(const char* _String) // Char 크기 계산
+{
+    int Len = 0;
+    for (int i = 0; _String[i] != 0; i += 1)
+    {
+        Len += 1;
+    }
+    return Len;
+}
+
+int CountFirst(const char* _Text, int _Start, const char* _FindStr)
+{
+    int _FindStrLen = CountLen(_FindStr);
+    bool find = false;
+    for (int i = _Start; _Text[i] != 0; i += 1)
+    {
+        find = true;
+        for (int j = i, k = 0; k != _FindStrLen && find != false; j += 1, k += 1)
+        {
+            if (_Text[j] != _FindStr[k])
+            {
+                find = false;
+            }
+        }
+        if (find == true)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int CountLast(const char* _Text, int _End, const char* _FindStr)
+{
+    int _FindStrLen = CountLen(_FindStr);
+    bool find = false;
+    for (int i = _End - _FindStrLen; i >= 0; i -= 1)
+    {
+        find = true;
+        for (int j = i, k = 0; k != _FindStrLen && find != false; j += 1, k += 1)
+        {
+            if (_Text[j] != _FindStr[k])
+            {
+                find = false;
+            }
+        }
+        if (find == true)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+int main()
+{
+    // 실패
+    {
+        int Count = CountFirst("aaa eee ttt asdfasd eee", 15, "eee");
+    }
+    {
+        int Count = CountLast("aaa eee asdfasd eee", 15, "eee");
+    }
+
+}
