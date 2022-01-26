f53f462d343207ab9f9884a8fc3d074026dbbc80
장중혁 <wndgur98@gmail.com>
2022-01-26 18:23:27 +0900

    장중혁 : HomeWork220126 업데이트

---
+++ HomeWork220126/HomeWork220126.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+class MyInt
+{
+public:
+    int Value;
+
+public:
+    MyInt(int _Value)
+        : Value(_Value)
+    {
+
+    }
+    MyInt operator ++()
+    {
+        Value += 1;
+        return *this;
+    }
+    MyInt operator ++(int _idx)
+    {
+        MyInt Idx(this->Value);
+        this->Value += 1;
+        return Idx;
+    }
+};
+
+int main()
+{
+    int Value = 0;
+    int Result = 0;
+
+    Result = ++Value;
+    Result = 0;
+    Result = Value++;
+
+
+    MyInt MyValue = 0;
+    MyInt MyResult = 0;
+
+    // ++ 쓰지마세요
+    //MyResult = ++MyValue;
+    //MyResult = 0;
+    //MyResult = MyValue++;
+}
