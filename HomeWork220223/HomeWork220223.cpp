d0e9c04c6eba5f462d4923ba452e8af9a54b1463
장중혁 <wndgur98@gmail.com>
2022-02-25 00:57:12 +0900

    장중혁 : HomeWork220223 업데이트

---
+++ HomeWork220223/HomeWork220223.cpp
@@ -0,0 +1,22 @@
+﻿// HomeWork220223.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+int Value = 0;
+void fucnt()
+{
+    Value += 1;
+    std::cout << Value << std::endl;
+    if (Value < 30)
+    {
+        fucnt();
+        
+    }
+}
+
+
+int main()
+{
+    fucnt();
+    
+}
