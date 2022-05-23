300db10ea28759c9fdddfa4dc4a767b28380c68c
wndgur98@gmail.com <wndgur98@gmail.com>
2022-05-23 16:48:01 +0900

    장중혁 : DXHomeWork 추가

---
+++ DXHomeWork/GameEngineCore/GameEngineCore_Resources.cpp
@@ -0,0 +1,115 @@
+#include "GameEngineCore.h"
+#include <GameEngineBase/GameEngineWindow.h>
+#include <GameEngineBase/GameEngineInput.h>
+#include <GameEngineBase/GameEngineTime.h>
+#include "GameEngineLevel.h"
+#include <math.h>
+
+// Resources Header
+#include "GameEngineVertexBuffer.h"
+#include "GameEngineIndexBuffer.h"
+
+void GameEngineCore::EngineResourcesInitialize()
+{
+	// 사각형 박스 에러용 텍스처 등등
+	// 엔진수준에서 기본적으로 지원줘야 한다고 생각하는
+	// 리소스들을 이니셜라이즈하는 단계
+
+
+	{
+		// 0       1
+
+		//    원점
+		
+		// 3       2
+
+		std::vector<float4> Vertex;
+		Vertex.push_back(float4(-0.5f, 0.5f));
+		Vertex.push_back(float4(0.5f, 0.5f));
+		Vertex.push_back(float4(0.5f, -0.5f));
+		Vertex.push_back(float4(-0.5f, -0.5f));
+
+		GameEngineVertexBuffer::Create("Rect", Vertex);
+	}
+
+	{
+		// 0       1
+
+		//    원점
+
+		// 3       2
+
+		std::vector<int> Index;
+
+		// 첫번째 삼각형
+		// 디폴트 생성자로 인자를 뒤에 추가해주는 요소 추가 함수.
+		Index.resize(6);
+
+		// 첫번째
+		Index[0] = 0;
+		Index[1] = 1;
+		Index[2] = 2;
+
+		// 두번째
+		Index[3] = 0;
+		Index[4] = 2;
+		Index[5] = 3;
+
+		GameEngineIndexBuffer::Create("Rect", Index);
+	}
+
+	{
+
+		float Idx = static_cast<float>(sqrt(3));
+		// 0       1
+
+		//    원점
+
+		// 3       2
+
+		std::vector<float4> Vertex;
+		Vertex.push_back(float4(0.0f, -2.0f));
+		Vertex.push_back(float4(Idx, 1.0f));
+		Vertex.push_back(float4(-Idx, 1.0f));
+		Vertex.push_back(float4(-Idx, -1.0f));
+		Vertex.push_back(float4(Idx, -1.0f));
+		Vertex.push_back(float4(0, 2.0f));
+
+		GameEngineVertexBuffer::Create("Hexagram", Vertex);
+	}
+
+	{
+		// 0       1
+
+		//    원점
+
+		// 3       2
+
+		std::vector<int> Index;
+
+		// 첫번째 삼각형
+		// 디폴트 생성자로 인자를 뒤에 추가해주는 요소 추가 함수.
+		Index.resize(6);
+
+		// 첫번째
+		Index[0] = 0;
+		Index[1] = 1;
+		Index[2] = 2;
+
+		// 두번째
+		Index[3] = 3;
+		Index[4] = 4;
+		Index[5] = 5;
+
+		GameEngineIndexBuffer::Create("Hexagram", Index);
+	}
+
+
+}
+
+
+void GameEngineCore::EngineResourcesDestroy()
+{
+	GameEngineVertexBuffer::ResourcesDestroy();
+	GameEngineIndexBuffer::ResourcesDestroy();
+}
