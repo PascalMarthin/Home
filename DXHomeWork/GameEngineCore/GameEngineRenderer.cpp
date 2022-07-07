83f48013b2f871e184593557374284f321f69147
wndgur98@gmail.com <wndgur98@gmail.com>
2022-07-07 19:18:06 +0900

    장중혁 : HomeWork220707

--- DXHomeWork/GameEngineCore/GameEngineRenderer.cpp
+++ DXHomeWork/GameEngineCore/GameEngineRenderer.cpp
@@ -1,3 +1,4 @@
+#include "PreCompile.h"
 #include "GameEngineRenderer.h"
 #include "GameEngineActor.h"
 #include "GameEngineLevel.h"
@@ -21,77 +22,46 @@ void GameEngineRenderer::Start()
 	GetActor()->GetLevel()->PushRenderer(this);
 }
 
-float4 XDir = { 1, 0 };
-float4 YDir = { 0, -1 };
-
-float Angle = 0.0f;
-float Dis = 0.0f;
-float4 Postion;
-
-void GameEngineRenderer::Render(float _DeltaTime)
-{
-	// 방향을 정했고.
-	// 나아가는 크기가 1이 아니라는 겁니다.
-	// _DeltaTime *= 200.0f;
-
-	// float4 Dir = float4::DegreeToDirection2D(Angle);
-
-	// x 100.0f = cosf(각도) * 빗변의 길이
-	// y 100.0f = sinf(각도) * 빗변의 길이
-
-	float4 Dir = {100.0f, 100.0f, 0.0f};
-
-	//float4::VectorRotationToRadianZ(Dir)
-
-	Angle += _DeltaTime * 360.0f;
-	Dis += _DeltaTime * 50.0f;
-
-	GetActor()->GetTransform().SetPosition(float4{300.0f, 300.0f});
-
-	// GetActor()->GetTransform().SetMove(Dir * _DeltaTime * 200.0f);
-
-	// 랜더링
-	GameEngineVertexBuffer* Vertex = GameEngineVertexBuffer::Find("Hexagram");
-	GameEngineIndexBuffer* Index = GameEngineIndexBuffer::Find("Hexagram");
-	
-	std::vector<POINT> DrawVertex;
-	DrawVertex.resize(Index->Indexs.size());
-
-	std::vector<float4> CopyBuffer;
-	CopyBuffer.resize(Index->Indexs.size());
-
-
-
-	for (size_t i = 0; i < Index->Indexs.size(); i++)
-	{
-		int TriIndex = Index->Indexs[i];
-
-		// 0 번째 순서의 점이 됩니다.
-		CopyBuffer[i] = Vertex->Vertexs[TriIndex];
-
-		// [0.5f] [0.5f] []                  [100] [100] [] 
-		// 크
-		CopyBuffer[i] *= GetActor()->GetTransform().GetScale();
-
-		// 자전
-		// CopyBuffer[TriIndex] *= GetActor()->GetTransform().GetScale();
-
-		CopyBuffer[i] = CopyBuffer[i].RotationToDegreeZ(Dis);
-
-		// 이동
-		CopyBuffer[i] += GetActor()->GetTransform().GetPosition();
-
-		
-
-		DrawVertex[i] = CopyBuffer[i].GetConvertWindowPOINT();
-	}
-
-	
-	for (size_t i = 0; i < DrawVertex.size(); i += 3)
-	{
-		Polygon(GameEngineWindow::GetHDC(), &DrawVertex[i], 3);
-	}
-	
-
-	// Rectangle(GameEngineWindow::GetHDC(), LeftTop.ix(), LeftTop.iy(), RightBot.ix(), RightBot.iy());
-}
+//void GameEngineRenderer::Render(float _DeltaTime)
+//{
+//	//// 랜더링
+//	////GameEngineVertexBuffer* Vertex = GameEngineVertexBuffer::Find("Rect");
+//	////GameEngineIndexBuffer* Index = GameEngineIndexBuffer::Find("Rect");
+//
+//	//GameEngineVertexBuffer* Vertex = GameEngineVertexBuffer::Find("Box");
+//	//GameEngineIndexBuffer* Index = GameEngineIndexBuffer::Find("Box");
+//	//
+//	//std::vector<POINT> DrawVertex;
+//	//DrawVertex.resize(Index->Indexs.size());
+//
+//	//std::vector<float4> CopyBuffer;
+//	//CopyBuffer.resize(Index->Indexs.size());
+//
+//
+//	//for (size_t i = 0; i < Index->Indexs.size(); i++)
+//	//{
+//	//	int TriIndex = Index->Indexs[i];
+//
+//	//	// 0 번째 순서의 점이 됩니다.
+//	//	// 최초에 원본 매쉬의 점을 복사합니다.
+//	//	CopyBuffer[i] = Vertex->Vertexs[TriIndex];
+//
+//	//	auto& tran = GetTransform();
+//
+//	//	// 버텍스쉐이더
+//	//	CopyBuffer[i] = CopyBuffer[i] * GetTransform().GetWorldViewProjection();
+//
+//
+//	//	// 레스터라이저
+//	//	//// 기록해놨던 z값으로 나뉘는것
+//	//	CopyBuffer[i] = CopyBuffer[i] / CopyBuffer[i].w;
+//
+//
+//	//	DrawVertex[i] = CopyBuffer[i].GetConvertWindowPOINT();
+//	//}
+//	//
+//	//for (size_t i = 0; i < DrawVertex.size(); i += 3)
+//	//{
+//	//	Polygon(GameEngineWindow::GetHDC(), &DrawVertex[i], 3);
+//	//}
+//}
